652c6031e88c0aeb302d5c869078da5c9b50c7f5
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-04-13 14:11:05 +0200

    create component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    render(){
+      return (
+      <div className="App">
+        <h1>chat</h1>
+        <img src="chat.png" alt="logo" />
+       
+      </div>
+    );
+    }
+  }
+export default App;
