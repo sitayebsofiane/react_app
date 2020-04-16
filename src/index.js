5d29464691725d6664a745b14f048f223171da15
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-04-16 14:01:08 +0200

    bruno

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './comonents/App';
 
-  const rootElement = document.getElementById("root")
-  ReactDOM.render(<App />,rootElement)
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />,rootElement)
